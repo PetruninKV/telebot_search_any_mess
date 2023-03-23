aa6319ae275c7111584ee26f2cc6460c1957a1e3
Kirill Petrunin <petruninkir@gmail.com>
2023-03-23 17:10:55 +0400

    added handler for other messages

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from config_data.config import Config, load_config
 from key_boards.main_menu import set_main_menu
-from handlers import user_handlers
+from handlers import user_handlers, other_handlers
 
 
 logger = logging.getLogger(__name__)
@@ -31,6 +31,7 @@ async def main():
     await set_main_menu(bot)
 
     dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
