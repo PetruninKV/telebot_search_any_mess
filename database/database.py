80db7be36e63cf2dab9c16c51911c263462bbd65
Kirill Petrunin <petruninkir@gmail.com>
2023-04-04 12:09:30 +0400

    refactoring

--- database/database.py
+++
@@ -1,7 +0,0 @@
-user_dict_template: dict = {'list_channels': set(),
-                            'list_keywords': set(),
-                            'work_on': False}
-
-
-
-users_db: dict[int, dict] = {}
